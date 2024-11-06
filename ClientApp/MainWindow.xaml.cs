f6bb2c52766bea745b1d993212446f8de4af2fc6
Shlyu_pl02@student.itstep.org <Shlyu_pl02@student.itstep.org>
2024-11-06 19:24:20 +0200

    class group chats

--- ClientApp/MainWindow.xaml.cs
+++ ClientApp/MainWindow.xaml.cs
@@ -55,6 +55,52 @@ namespace ClientApp
         }
     }
 
+    //public class User
+    //{
+    //    public string Name { get; set; }
+    //    public string Email { get; set; }
+    //    public string Country { get; set; }
+    //    public string Phone { get; set; }
+    //    public string Passport { get; set; }
+
+    //    public User(string name, string email, string country, string phone, string passport)
+    //    {
+    //        Name = name;
+    //        Email = email;
+    //        Country = country;
+    //        Phone = phone;
+    //        Passport = passport;
+    //    }
+    //}
+    public class GroupChat
+    {
+        public int Id { get; set; }
+        public List<User> Users { get; set; } = new List<User>();
+        public DateTime Time { get; set; }
+        public string Data { get; set; }
+        public User Admin { get; set; }
+        public string Text { get; set; }
+        public GroupChat(int id, User admin, string text, DateTime time, string data = null)
+        {
+            Id = id;
+            Admin = admin;
+            Text = text;
+            Time = time;
+            Data = data;
+        }
+        public void AddUser(User user)
+        {
+            Users.Add(user);
+        }
+        public void RemoveUser(User user)
+        {
+            Users.Remove(user);
+        }
+        public void UpdateText(string newText)
+        {
+            Text = newText;
+        }
+    }
     public class User
     {
         public string Name { get; set; }
@@ -62,7 +108,6 @@ namespace ClientApp
         public string Country { get; set; }
         public string Phone { get; set; }
         public string Passport { get; set; }
-
         public User(string name, string email, string country, string phone, string passport)
         {
             Name = name;
