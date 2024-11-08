1a2e8aa306c528997ff5344f1d2c2b7bb05cb7c5
Merku_nw35@student.itstep.org <Merku_nw35@student.itstep.org>
2024-11-08 18:39:06 +0200

    class contact list

--- ClientApp/MainWindow.xaml.cs
+++ ClientApp/MainWindow.xaml.cs
@@ -122,28 +122,28 @@ namespace ClientApp
             Passport = passport;
         }
     }
-    public class ContactList
-    {
-        private List<User> users = new List<User>();
+    //public class ContactList
+    //{
+    //    private List<User> users = new List<User>();
 
-        public void AddUser(User user)
-        {
-            users.Add(user);
-        }
+    //    public void AddUser(User user)
+    //    {
+    //        users.Add(user);
+    //    }
 
-        public void RemoveUser(User user)
-        {
-            users.Remove(user);
-        }
+    //    public void RemoveUser(User user)
+    //    {
+    //        users.Remove(user);
+    //    }
 
-        public void PrintAllUsers()
-        {
-            foreach (User user in users)
-            {
-                Console.WriteLine($"Name: {user.Name}, Email: {user.Email}, Country: {user.Country}, Phone: {user.Phone}, Passport: {user.Passport}");
-            }
-        }
-    }
+    //    public void PrintAllUsers()
+    //    {
+    //        foreach (User user in users)
+    //        {
+    //            Console.WriteLine($"Name: {user.Name}, Email: {user.Email}, Country: {user.Country}, Phone: {user.Phone}, Passport: {user.Passport}");
+    //        }
+    //    }
+    //}
 
 }
 
